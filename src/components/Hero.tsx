907515a7ec033530e44b94b5bcc896e536e10699
kwangkwangkim00 <kwangkwangkim00@gmail.com>
2026-08-14 07:02:56 +0900

    Hero: 프로필/강의/책 사진 레이어드 콜라주(PhotoCollage)로 교체
    - 강의 사진 최대 6장 자동 크로스페이드, 프로필/책 사진은 고정
    - 스크롤 패럴랙스 + 아이들 플로트 애니메이션 (transform/opacity만)
    - 사진 없으면 기존 ProfilePhoto와 동일한 플레이스홀더 표시

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { motion, useScroll, useTransform } from "motion/react";
 import { useRef } from "react";
-import ProfilePhoto from "./ProfilePhoto";
+import PhotoCollage from "./PhotoCollage";
 
 const CREDENTIALS = [
   "서울가족학교 예비부부교실 위촉강사",
@@ -93,7 +93,7 @@ export default function Hero() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.7, delay: 0.35 }}
         >
-          <ProfilePhoto />
+          <PhotoCollage />
         </motion.div>
       </div>
 
